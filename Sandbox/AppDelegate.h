40e40be7ee3e9261981523cc8b15196b4d7ac30e
David Lopez Rodriguez <dloprodu@gmail.com>
2017-10-11 19:15:57 +0100

    First commit

---
+++ Sandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sandbox
+//
+//  Created by David Lopez Rodriguez on 11/10/2017.
+//  Copyright © 2017 David López Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sandbox
+//
+//  Created by David Lopez Rodriguez on 11/10/2017.
+//  Copyright © 2017 David López Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
